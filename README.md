629fccc39e9a2ec08b4fbdbcce6813dc7190c03f
max <biryukov.maxim.vladimirovich@yandex.ru>
2020-03-13 01:38:06 +0300

    step 4 w/o pipeline

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # backend-project-lvl1
+[![Maintainability](https://api.codeclimate.com/v1/badges/63d4c778bfb6c609b8df/maintainability)](https://codeclimate.com/github/biryukovmaxim/backend-project-lvl1/maintainability)
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const askName = () => {
-    const answer = readlineSync.question('May I have your name?: ');
-    console.log(`Hello, ${answer}!`)
+const askName = () => {
+  const answer = readlineSync.question('May I have your name?: ');
+  console.log(`Hello, ${answer}!`);
 };
+
+export default askName;
