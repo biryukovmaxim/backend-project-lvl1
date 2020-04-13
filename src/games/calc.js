a950ad4890f69323a2873d2bc38cfd4784abadc6
Maxim Biryukov <biryukov.maxim.vladimirovich@yandex.ru>
2020-04-13 16:57:19 -0700

    new structure of gameCalc

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,37 +1,17 @@
 import startEngine from '../index.js';
 import random from '../random.js';
 
-const numToSym = (num) => {
-  switch (num) {
-    case 0:
-      return '+';
-    case 1:
-      return '-';
-    case 2:
-      return '*';
-    default:
-      break;
-  }
-  return NaN;
-};
+const operations = ['-', '+', '*'];
 
 const gameCalc = () => {
-  const params = [random(0, 100), numToSym(random(0, 2)), random(0, 100)];
-  let result;
-  switch (params[1]) {
-    case '+':
-      result = params[0] + params[2];
-      break;
-    case '-':
-      result = params[0] - params[2];
-      break;
-    case '*':
-      result = params[0] * params[2];
-      break;
-    default:
-      return 'error';
-  }
-  return [params.join(' '), String(result)];
+  const [num1, operationSign, num2] = [random(0, 100), operations[random(0, 2)], random(0, 100)];
+  const mathExpression = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+  };
+
+  return [`${num1} ${operationSign} ${num2}`, String(mathExpression[operationSign](num1, num2))];
 };
 const description = 'What is the result of the expression?';
 
