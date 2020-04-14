68b01d067a2abc72d7323cb4a0ab410d2c0295c7
Maxim Biryukov <biryukov.maxim.vladimirovich@yandex.ru>
2020-04-13 17:02:54 -0700

    -space lane

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import readlineSync from 'readline-sync';
 
-
 const gameEngine = (description, gameFunc) => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name?: ');
