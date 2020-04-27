16d7cf93d2a24f49b252868fc5b93e18fc2383e3
Maxim Biryukov <biryukov.maxim.vladimirovich@yandex.ru>
2020-04-27 22:27:02 +0300

    delete \n

--- src/games/calc.js
+++ src/games/calc.js
@@ -3,23 +3,19 @@ import getRandomInt from '../utils.js';
 
 const description = 'What is the result of the expression?';
 
-
-const mathExpressions = [
-  { getResult: (a, b) => a + b, operationSym: '+' },
-  { getResult: (a, b) => a - b, operationSym: '-' },
-  { getResult: (a, b) => a * b, operationSym: '*' },
-];
-
-const operationCount = mathExpressions.length;
+const getResult = {
+  '+': (a, b) => a + b,
+  '-': (a, b) => a - b,
+  '*': (a, b) => a * b,
+};
 
 const getGameData = () => {
   const num1 = getRandomInt(0, 100);
   const num2 = getRandomInt(0, 100);
-  const operationID = getRandomInt(0, operationCount - 1);
-  const operationSign = mathExpressions[operationID].operationSym;
-  getRandomInt(0, 100);
+  const randomIndexOfSign = getRandomInt(0, Object.keys(getResult).length - 1);
+  const operationSign = Object.keys(getResult)[randomIndexOfSign];
   const question = `${num1} ${operationSign} ${num2}`;
-  const answer = String(mathExpressions[operationID].getResult(num1, num2));
+  const answer = String(getResult[operationSign](num1, num2));
   return [question, answer];
 };
 
